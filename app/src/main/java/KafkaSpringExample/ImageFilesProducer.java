665219eab5b9fbddff0d1ce98c003441249c390b
DSkilton <MC03353@mbro.ac.uk>
2022-07-18 21:51:59 +0100

    Adding further functionality

--- app/src/main/java/KafkaSpringExample/ImageFilesProducer.java
+++ app/src/main/java/KafkaSpringExample/ImageFilesProducer.java
@@ -1,9 +1,26 @@
 package KafkaSpringExample;
 
+import java.lang.System.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
 /**
  *
  * @author MC03353
  */
+
+@Component
 public class ImageFilesProducer {
+    static Logger logger = LoggerFactory.getLogger(ImageFilesProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, ImageFile> producer;
+    
+    @Value("${spring.cloud.stream.bindings.input1.destination}")
+    private String topic;
+
     
 }
