5398787bd9103755c906674c61c7013f18858053
DSkilton <MC03353@mbro.ac.uk>
2022-07-20 20:39:06 +0100

    Correct imports for ImageFilesProducer and began to add more functionality to ProducerCallBack

--- app/src/main/java/KafkaSpringExample/ImageFilesProducer.java
+++ app/src/main/java/KafkaSpringExample/ImageFilesProducer.java
@@ -1,9 +1,9 @@
 package KafkaSpringExample;
 
-import com.google.common.io.Files;
 import java.io.IOException;
 import java.lang.System.Logger;
 import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
