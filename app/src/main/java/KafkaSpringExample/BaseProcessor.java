0dc1d54469c507b9588d48ad61ddd6c627c0e9b0
DSkilton <MC03353@mbro.ac.uk>
2022-07-22 21:15:35 +0100

    Debugged BaseProcessor

--- app/src/main/java/KafkaSpringExample/BaseProcessor.java
+++ app/src/main/java/KafkaSpringExample/BaseProcessor.java
@@ -20,42 +20,42 @@ import org.springframework.kafka.core.ProducerFactory;
 
 /**
  *
- * @author MC03353
- * Base processor, responsible for defining stream configuration
+ * @author MC03353 Base processor, responsible for defining stream configuration
  */
-public abstract class BaseProcessor {s
+public abstract class BaseProcessor {
+
     @Value("${bootstrap.servers}")
     private String bootstrapServers;
-    
+
     @Bean
-    public Map<String, Object> processorProducerConfigs(){
+    public Map<String, Object> processorProducerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-        
-        return props;        
+
+        return props;
     }
-    
+
     @Bean
-    public ProducerFactory<Integer, Integer> processorProducerFactory(){
+    public ProducerFactory<Integer, Integer> processorProducerFactory() {
         return new DefaultKafkaProducerFactory<>(processorProducerConfigs());
     }
-    
+
     @Bean
-    public KafkaTemplate<Integer, Integer> processorProducerFactory(){
+    public KafkaTemplate<Integer, Integer> producerTemplate() {
         return new KafkaTemplate<>(processorProducerFactory());
     }
-    
+
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    public KafkaStreamsConfiguration streamConfigs(){
+    public KafkaStreamsConfiguration streamConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "image-files-procesor");
-        
+
         return new KafkaStreamsConfiguration(props);
     }
 }
