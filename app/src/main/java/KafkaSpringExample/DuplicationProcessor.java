327c81076c52dc20411e9bf6bd23af584b5e0d46
DSkilton <MC03353@mbro.ac.uk>
2022-07-24 14:52:07 +0100

    completed detectDuplicateImages method

--- app/src/main/java/KafkaSpringExample/DuplicationProcessor.java
+++ app/src/main/java/KafkaSpringExample/DuplicationProcessor.java
@@ -4,6 +4,7 @@
  */
 package KafkaSpringExample;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,6 +17,10 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.UnlimitedWindows;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -43,6 +48,9 @@ public class DuplicationProcessor extends BaseProcessor {
 
     @Value("$spring.cloud.stream.bindings.input1.destination}")
     private String input1;
+    
+    @Value("$spring.cloud.stream.bindings.output1.destination")
+    private String output1;
 
     @Bean
     public KStream<String, ImageFile> detectDuplicateImages(StreamsBuilder streamsBuilder) {
@@ -56,12 +64,37 @@ public class DuplicationProcessor extends BaseProcessor {
                         return new KeyValue<>(0L, imageFile.getFileName());
                     }
                 })
-                .groupByKey(Grouped.with(Serdes.Long(),Serdes.String()).withName("g1"))
+                .groupByKey(Grouped.with(Serdes.Long(), Serdes.String()).withName("g1"))
                 .reduce((value1, value2) -> {
                     Set<String> s1 = new HashSet<String>(Arrays.asList(value1.split("\\|")));
                     s1.addAll(new HashSet<String>(Arrays.asList(value2.split("\\|"))));
                     return String.join("|", s1);
-                });
+                }, Materialized.with(Serdes.Long(), Serdes.String()))
+                .toStream()
+                .filter((Key, value) -> Strings.isNotEmpty(value) && value.contains("|"))
+                .flatMapValues((key, value) -> Arrays.asList(value.split("\\|")))
+                .groupByKey(Grouped.with(Serdes.Long(), Serdes.String()).withName("g2"))
+                .windowedBy(UnlimitedWindows.of())
+                .reduce((file1, file2) -> {
+                    try {
+                        String comp1 = file1;
+                        if (file1.contains("|")) {
+                            comp1 = file1.split("\\|")[0];
+                        }
+                        if (!com.google.common.io.Files.equal(new File(comp1), new File(file2))) {
+                            return null;
+                        } else {
+                            return String.join("|", file1, file2);
+                        }
+                    } catch (IOException e) {
+                        return null;
+                    }
+                }, Materialized.with(Serdes.Long(),Serdes.String()))
+                .toStream()
+                .filter((key, value) -> Strings.isNotEmpty(value) && value.contains("|"))
+                .map((key, value) -> new KeyValue<>(key.key(), value))
+                .to(output1, Produced.with(Serdes.Long(), Serdes.String()));
+        ;
         return imageFileStream;
     }
 
